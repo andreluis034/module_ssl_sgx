4e1d6a6da776515e5c37983d5b6f51da5c4b61cf
Andre <andre.brandao@fc.up.pt>
2020-04-14 15:56:53 +0100

    Fix include issues of WolfSSL & compatibilityLayer

--- Include/user_types.h
+++ Include/user_types.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <inttypes.h>
+#define _SSL_MAX_SSL_SESSION_ID_LENGTH 32
 typedef uint64_t WOLFSSL_SSL_IDENTIFIER;
 typedef uint64_t WOLFSSL_SSL_CTX_IDENTIFIER;
 typedef uint64_t WOLFSSL_X509_IDENTIFIER;
@@ -13,40 +14,6 @@ typedef uint64_t WOLFSSL_509_STORE_CTX_IDENTIFIER;
 typedef uint64_t WOLFSSL_EC_GROUP_IDENTIFIER;
 typedef uint64_t WOLFSSL_OCSP_RESPONSE_IDENTIFIER;
 typedef uint64_t WOLFSSL_OCSP_REQUEST_IDENTIFIER;
+typedef uint64_t WOLFSSL_X509_NAME_ENTRY_IDENTIFIER;
+typedef uint64_t WOLFSSL_X509_NAME_IDENTIFIER;
 
-
-typedef WOLFSSL_SSL_IDENTIFIER SSL;
-typedef WOLFSSL_SSL_CTX_IDENTIFIER SSL_CTX;
-typedef WOLFSSL_X509_IDENTIFIER X509;
-typedef WOLFSSL_X509_CTX_IDENTIFIER X509;
-typedef WOLFSSL_DH_IDENTIFIER DH;
-typedef WOLFSSL_EVP_PKEY_IDENTIFIER EVP_PKEY;
-typedef WOLFSSL_SESSION_IDENTIFIER SSL_SESSION;
-typedef WOLFSSL_BIO_IDENTIFIER BIO;
-typedef WOLFSSL_509_STORE_IDENTIFIER X509_STORE;
-typedef WOLFSSL_509_STORE_CTX_IDENTIFIER X509_STORE_CTX;
-typedef WOLFSSL_EC_GROUP_IDENTIFIER EC_GROUP;
-typedef WOLFSSL_OCSP_RESPONSE_IDENTIFIER OCSP_RESPONSE;
-typedef WOLFSSL_OCSP_REQUEST_IDENTIFIER OCSP_REQUEST;
-
-
-#define _X509_V_OK 0
-
-typedef int (*stack_cmp_func)(const void **a, const void **b);
-typedef struct stack_st {
-  /* num contains the number of valid pointers in |data|. */
-  unsigned int num;
-  void **data;
-  /* sorted is non-zero if the values pointed to by |data| are in ascending
-   * order, based on |comp|. */
-  unsigned int sorted;
-  /* num_alloc contains the number of pointers allocated in the buffer pointed
-   * to by |data|, which may be larger than |num|. */
-  unsigned int num_alloc;
-  /* comp is an optional comparision function. */
-  stack_cmp_func comp;
-} _STACK;
-
-#define STACK_OF(type) struct stack_st_##type
-
-#define DEFINE_STACK_OF(type) STACK_OF(type) {_STACK stack;};
